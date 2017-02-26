32b9c6ce47596c987f5000016e71df26de987f72
Brett <bglidden@calpoly.edu>
2017-02-25 21:54:02 -0800

    working barometer files added

--- barometer.cpp
+++ barometer.cpp
@@ -1,44 +1,61 @@
-#include <barometer.h>
+#include "barometer.h"
 #include <Wire.h>
 #include <math.h>
 
+// Barometer Configuration
+#define BARO_PRESSURE 	(0x48)
+#define BARO_TEMP 		(0x58)
+#define BARO_ADDRESS 	(0x77)
+#define BARO_RESET 		(0x1E)
+#define BARO_PROM_READ 	(0xA2)
 
 // calibration data
 uint16_t C[7] = {0};
+int32_t dT;
 
 // Initialize barometer from unknown state
-void initBarometer(void) {
+void initMS5611(void) {
 	// reset the barometer
 	Wire.beginTransmission(BARO_ADDRESS);
 	Wire.write(BARO_RESET);
 	Wire.endTransmission();
-	
+	delay(10);
+ 
 	for(uint8_t i = 0; i < 6; i++) {
 		// write into PROM data
 		Wire.beginTransmission(BARO_ADDRESS);
-		Wire.write(BARO_PROM_READ + (i << 1));
+		Wire.write(BARO_PROM_READ + (i * 2));
 		Wire.endTransmission();
 		
 		// read calibration data
 		Wire.requestFrom(BARO_ADDRESS, (uint8_t) 2);
-		while(Wire.available() == 0)
-			;
+		while(Wire.available() == 0);
+   
 		C[i+1] = (Wire.read() << 8) | Wire.read();
+    SerialUSB.print(C[i+1]);
 	}
+ delay(500);
 	return;
 }
 
-// Primes barometer to organize relevant data for reading
-void primeBarometer(uint8_t data_to_get) {
+// Primes MS5611 to organize temperature data for reading
+void primeTempMS5611(void) {
 	Wire.beginTransmission(BARO_ADDRESS);
-	Wire.write(data_to_get);
+	Wire.write(BARO_TEMP);
 	Wire.endTransmission();
 	return;
 }
 
+// Primes MS5611 to orgainze pressure data for reading
+void primePressureMS5611(void) {
+    Wire.beginTransmission(BARO_ADDRESS);
+	Wire.write(BARO_PRESSURE);
+	Wire.endTransmission();
+	return;
+}
 
 // Reads the raw values based on the previous priming method (Temp or Pressure)
-uint32_t readBarometerRaw() {
+uint32_t readRawMS5611(void) {
 	Wire.beginTransmission(BARO_ADDRESS);
 	Wire.write(0);
 	Wire.endTransmission();
@@ -49,34 +66,63 @@ uint32_t readBarometerRaw() {
 	return (((uint32_t)Wire.read() << 16) | ((uint32_t)Wire.read() << 8) | (uint32_t)Wire.read());
 }
 
-// Updates barometer struct with new readings
-// uses datasheet methods to obtain pressure, temperature, and alititude readings
-// tons of fun going on here with magic numbers from datasheet
-void updateBaromResults(struct BaroData * data) {
-	uint32_t raw_pressure;
+/* Reads MS5611 temperature data into MS5611data struct
+ * Assumptions:
+ *     MS5611 primed for temperature
+ *     10 ms delay after priming
+ */
+void readTempMS5611(struct MS5611data *data) {
 	uint32_t raw_temp;
-	int32_t dT;
-	int32_t TEMP;
-	int64_t OFF;
-	int64_t SENS;
-	int32_t P;
-	
-	primeBarometer(BARO_PRESSURE);
-	raw_pressure = readBarometerRaw();
-	
-	primeBarometer(BARO_TEMP);
-	raw_temp = readBarometerRaw();
+	int32_t temp;
+	///int32_t dT;
+ 	
+	raw_temp = readRawMS5611();
 	
 	dT = int32_t(raw_temp - (((uint32_t)C[5]) << 8));
-	off = ((uint64_t)C[2] << 16) + (((int64_t)dT * (uint64_t)C[4]) >> 7);
-	sens = ((uint64_t)C[1] << 15) + (((int64_t)dT * (uint64_t)C[3]) >> 8);
 	
-	temp = ((int64_t)dT * (uint64_t)C[6]) >> 23;
-	temp = temp + 2000;
+	data->temperature = ((((int64_t)dT * (uint64_t)C[6]) >> 23) + 2000) / 100.0;
+
+    return;
+}
+
+/* Reads MS5611 pressure data into MS5611data struct
+ * Assumptions:
+ *    MS5611 primed for pressure
+ *    10 ms delay after priming
+*/
+void readPressureMS5611(struct MS5611data *data) {
+    uint32_t raw_pressure;
+    int64_t off;
+    int64_t sens;
+    //int32_t dT;
+
+    //dT = ((data->temperature * 100.0) - 2000) / ((uint64_t)C[6] >> 23)
+
+    raw_pressure = readRawMS5611();
+
+    off  = ((uint64_t)C[2] << 16) + (((int64_t)dT * (uint64_t)C[4]) >> 7);
+    sens = ((uint64_t)C[1] << 15) + (((int64_t)dT * (uint64_t)C[3]) >> 8);
+ 
+    data->pressure = (int32_t)(((((uint64_t)raw_pressure * sens) >> 21) - off) >> 15);
+  
+
+    //off = ((uint64_t)C[2] << 16) + (((int64_t)dT * (uint64_t)C[4]) >> 7);
+	//sens = ((uint64_t)C[1] << 15) + (((int64_t)dT * (uint64_t)C[3]) >> 8);
 	
-	P = (int32_t)(((((uint64_t)raw_pressure * sens) >> 21) - off) >> 15);
-	data->Temperature = temp / 100.0;
-	data->Pressure = P / 100.0;
-	data->Altitude = ((pow((1013.25 / Pressure), 1/5.257) - 1.0) * (Temperature + 273.15)) / 0.0065;
-	return;
+    //data->pressure = ((int32_t)(((((uint64_t)raw_pressure * sens) >> 21) - off) >> 15) / 100.0);
+    data->pressure = data->pressure / 100.0;
+    return;
+}
+
+// Calculate altitude using data from MS5611data struct
+void calcAltitudeMS5611(struct MS5611data *data) {
+    data->altitude = ((pow((1013.25 / data->pressure), 1/5.257) - 1.0) * (data->temperature + 273.15)) / 0.0065;
+}
+
+// Convert MS5611 struct data to string for output
+String ms5611ToString(struct MS5611data *data) {
+    String out = "Pressure = " + String(data->pressure);
+    out += "\nTemperature = " + String(data->temperature);
+    out += "\nAltitude = " + String(data->altitude);
+    return out;
 }
--- barometer.h
+++ barometer.h
@@ -3,25 +3,56 @@
 
 #include <Arduino.h>
 
-// Barometer Configuration
-#define BARO_PRESSURE 	(0x48)
-#define BARO_TEMP 		(0x58)
-#define BARO_ADDRESS 	(0x77)
-#define BARO_RESET 		(0x1E)
-#define BARO_PROM_READ 	(0xA2)
+struct MS5611data {
+    float temperature;
+	float pressure;
+	float altitude;
+};
 
-struct BaroData {
-	float Temperature;
-	float Pressure;
-	float Altitude;
-}
+// initialize MS5611 into operating state and collect calibration data 
+void initMS5611(void);
 
-// Main methods to call
-void initBarometer(void);
-void updateBaromResults(struct BaroData * data);
+/*--- Begin Temperature Section ---*/
 
-// Fine tuning
-void primeBarometer(uint8_t data_to_get);
+// Primes MS5611 to organize temperature data for reading
+void primeTempMS5611(void); 
+
+/* Reads MS5611 temperature data into MS5611data struct
+ * Assumptions:
+ *     MS5611 primed for temperature
+ *     10 ms delay after priming
+ */
+void readTempMS5611(struct MS5611data *data); 
+
+/*--- End Temperature Section ---*/    
+
+    
+/*--- Begin Pressure Section ---*/  
+    
+// Primes MS5611 to orgainze pressure data for reading
+void primePressureMS5611(void); 
+
+/* Reads MS5611 pressure data into MS5611data struct
+ * Assumptions:
+ *    MS5611 primed for pressure
+ *    10 ms delay after priming
+*/
+void readPressureMS5611(struct MS5611data *data); 
+
+/*--- End Pressure Section ---*/
+
+
+// Calculate altitude using data from MS5611data struct
+void calcAltitudeMS5611(struct MS5611data *data); 
+
+// Convert MS5611 struct data to string for output
+String ms5611ToString(struct MS5611data *data); 
+
+/* Reads the raw values based on the previous priming method (Temp or Pressure)
+ * ONLY CALL THIS IF YOU MUST!
+ * IT IS ALREADY IMPLEMENTED IN readTempMS6511 and readPressureMS5611
+*/
 uint32_t readBarometerRaw();
+
 
 #endif
