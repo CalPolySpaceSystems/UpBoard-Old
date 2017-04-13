af39cfea26fe77bedd98ca1b45c85222b24e2cb9
Patrick Chizek <patrick.chizek@comcast.net>
2017-04-13 15:46:46 -0700

    Cleaned up code to remove all control stuff

--- UpBoard_Telemetry_Only/UpBoard_Telemetry_Only.ino
+++ UpBoard_Telemetry_Only/UpBoard_Telemetry_Only.ino
@@ -131,97 +131,9 @@ void loop() {
   //SerialUSB.println("GPS");
   readLSM(&ldata);
   //SerialUSB.println("LSM");
-  //launched = launchDetect(&ldata, launched);
-  
-  //if (millis() > 20000)
-  //{
-    //launched = true;
-  //}
-
-  //state.launched = launched;
   
   timer0 = millis(); //time right before data processing
   
-  /*
-   
-  if (!launched)
-  {
-    //SerialUSB.println("Not Launched");
-    // Do nothing
-    //sensorMovingAverage();
-    for (int j = 0; j < 3; j++)
-    {
-      prevAngle[j] = state.angle[j];
-    }//memcpy(prevAngle, state.angle, 12);
-    prevData = ldata;
-  }
-  else
-  {
-    //ApplyOffsets(&ldata);
-    dt = (timer0 - timer1) / 1000;
-    //SerialUSB.println(dt);
-    
-    state.dt = dt;
-    
-    for (int j = 0; j < 3; j++)
-    {
-      state.angle[j] = compFilter(prevAngle[j], ldata.gyr[j], 0.0, dt); // Try passing 0 into the code
-    }
-    for (int j = 0; j < 3; j++)
-    {
-      prevAngle[j] = state.angle[j];
-    }//memcpy(prevAngle, state.angle, 12);
-    prevData = ldata; //set to keep previous data on next loop
-    
-  }
-
-  /
-  float pitchRaw = kd * ldata.gyr[0] + kp * state.angle[0];
-  float yawRaw = kd * ldata.gyr[1] + kp * state.angle[1];
-  if (pitchRaw > 15) {
-    pitchRaw = 15;
-  }
-  if (pitchRaw < -15) {
-    pitchRaw = -15;
-  }
-  if (yawRaw > 15) {
-    yawRaw = 15;
-  }
-  if (yawRaw < -15) {
-    yawRaw = -15;
-  }
-  
-  pitchArr[controlCounter] = pitchRaw;
-  yawArr[controlCounter] = yawRaw;
-  controlCounter++;
-  if (controlCounter >= 10)
-  {
-    controlCounter = 0;
-  }
-if (timer1 - timerServo > 100)
-{
-  //SerialUSB.println("Move");
-  for (int ii = 0; ii < 10; ii++)
-  {
-    pitch =+ pitchArr[ii];
-    yaw =+ yawArr[ii];
-  }
-  //float pitchMicro1 = 1500 + 60*.707*pitch + 30*.707*yaw;
-  //float pitchMicro2 = 1500 - 30*.707*pitch + 30*.707*yaw;
-  //float yawMicro1 = 1500 + 60*.707*yaw + 30*.707*pitch;
-  //float yawMicro2 = 1500 - 30*.707*yaw + 30*.707*pitch;
-//  pitch1.writeMicroseconds(pitchMicro1);
-//  pitch2.writeMicroseconds(pitchMicro2);
-//  yaw1.writeMicroseconds(yawMicro1);
-//  yaw2.writeMicroseconds(yawMicro2);
-  //  pitch1.write(pitch);
-  //  pitch2.write(-pitch);
-  //  yaw1.write(yaw);
-  //  yaw2.write(-yaw);
-  //SerialUSB.println(timer0-timer1, 4);
-  //timerServo = millis();
-}
-  */
   timer1 = timer0; //set to keep previous time on next loop
 
   while (timer0 + 10 < millis());
@@ -233,30 +145,10 @@ if (timer1 - timerServo > 100)
 
   Serial1.print(out);
   SerialUSB.println(out);
+
+  SerialUSB.println();
+  
   /*
-  SerialUSB.print(pitch);
-  SerialUSB.print("\t");
-  SerialUSB.print(yaw);
-  SerialUSB.print("\t");
-  SerialUSB.print(ldata.acc[0]);
-  SerialUSB.print("\t");
-  SerialUSB.print(ldata.acc[1]);
-  SerialUSB.print("\t");
-  SerialUSB.print(ldata.acc[2]);
-  SerialUSB.print("\t");
-  SerialUSB.print(ldata.gyr[0]);
-  SerialUSB.print("\t");
-  SerialUSB.print(ldata.gyr[1]);
-  SerialUSB.print("\t");
-  SerialUSB.print(ldata.gyr[2]);
-  SerialUSB.print("\t");
-  SerialUSB.print(state.angle[0]);
-  SerialUSB.print("\t");
-  SerialUSB.print(state.angle[1]);
-  SerialUSB.print("\t");
-  SerialUSB.print(state.angle[2]);
-  SerialUSB.print("\t");
-  SerialUSB.println(state.dt, 4);
   ourlog = SD.open("uplog.txt", FILE_WRITE);
     if(!ourlog) {
     SerialUSB.println("FILE ERROR");
@@ -268,65 +160,3 @@ if (timer1 - timerServo > 100)
   */
   delay(1);
 }
-/*
-float compFilter(float oldAngle, float gyroData, float accAng, float dt)
-//{
-  //return 0.99 * (oldAngle + gyroData * dt) + 0.01 * accAng;
-//}
-
-void ApplyOffsets(struct LSMData *ldata)
-{
-  ldata->acc[0] = ldata->acc[0] - offset.acc[0];
-  ldata->acc[1] = ldata->acc[1] - offset.acc[1];
-  ldata->acc[2] = ldata->acc[2] - offset.acc[2];
-  ldata->gyr[0] = ldata->gyr[0] - offset.gyr[0];
-  ldata->gyr[1] = ldata->gyr[1] - offset.gyr[1];
-  ldata->gyr[2] = ldata->gyr[2] - offset.gyr[2];
-}
-bool launchDetect(struct LSMData *data, bool launched)
-{
-  if (data->acc[2] > 1.5 || launched) //TODO: Determine what is a reasonable value for this
-  {
-    //SerialUSB.println("Launch Detected");
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-
-}
-
-void sensorMovingAverage()
-{
-  static struct LSMData total;
-  static int i;
-  // Subtract the last reading of the current index
-  for (int j = 0; j < 3; j++)
-  {
-    total.acc[j] -= readings[i].acc[j];
-    total.gyr[j] -= readings[i].gyr[j];
-  }
-  // Get new reading at current index
-  readLSM(&readings[i]);
-  for (int j = 0; j < 3; j++)
-  {
-    total.acc[j] += readings[i].acc[j];
-    total.gyr[j] += readings[i].gyr[j];
-  }
-  i++;
-  // Reset array count if array is full
-  if (i >= numReadings)
-  {
-    i = 0;
-  }
-  // Take the average of all the readings
-  for (int j = 0; j < 3; j++)
-  {
-    offset.acc[j] = total.acc[j] / numReadings;
-    offset.gyr[j] = total.gyr[j] / numReadings;
-  }
-  offset.acc[2] -= 1; //We should read 1 G in the z axis
-
-}
-*/
